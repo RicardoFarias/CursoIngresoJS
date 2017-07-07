0c6fea46cf729bf49697ff442891ce0e156d6db3
RicardoFarias <rjfariasv@gmail.com>
2017-07-07 00:20:34 -0300

    2 unidades listas

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,23 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numero;
+	numero= Math.random() * 10;
+	numero= Math.floor(numero);
+	if (numero>8)
+	{
+	alert (numero + "EXCELENTE");
+	}
+		else
+		{
+			if (numero>3)
+			{
+			alert (numero + "APROBO");
+			}
+			else
+			{
+			alert (numero + "Vamos la proxima se puede");
+			}
+		}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,9 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numero;
+numero= Math.random() * 10;
+numero= Math.floor(numero);
+alert (numero);
 
 }//FIN DE LA FUNCIÓN
