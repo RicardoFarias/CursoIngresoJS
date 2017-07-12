b65cc718ec9d8bc7ef66fee157aa50f11b750e09
RicardoFarias <rjfariasv@gmail.com>
2017-07-11 21:13:48 -0300

    hasta el 9

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -5,39 +5,39 @@ function Mostrar()
 	var numero;
 	var maximo;
 	var minimo;
+	var bandera=true;
 	var respuesta='si';
 
 	while(respuesta!='no')
 	{
-		
-	numero=prompt ("ingrese un mumero");
-	numero=parseInt(numero);
-	while (isNaN (numero))
-	{
-		numero=prompt ("Error! Ingrese un numero");
+		numero=prompt ("ingrese un mumero");
 		numero=parseInt(numero);
-	}
-	if (numero>maximo)
-	{
-	maximo= numero;
-	contador++;	
-	}
-	else
-	{
-		if (numero<minimo)
+		while (isNaN (numero))
 		{
-		minimo=numero;
-		contador++;	
+			numero=prompt ("Error! Ingrese un numero");
+			numero=parseInt(numero);
 		}
-	}
-	respuesta=prompt ("desea continuar");
-}	
-document.getElementById('maximo').value=maximo;
-document.getElementById('minimo').value=minimo;
+			if (bandera)
+			{
+				maximo= numero;
+				minimo=numero;
+				bandera=false;	
+			}
+			else
+			{
+				if (numero>maximo)
+				{
+					maximo=numero;
+					contador++;	
+				}
+				if (numero<minimo)
+				{
+					minimo=numero;
+				}
+			}
+		respuesta=prompt ("desea continuar");
+	}	
+	document.getElementById('maximo').value=maximo;
+	document.getElementById('minimo').value=minimo;
 	
-	}
-
-
-
-
-}//FIN DE LA FUNCIÓN
+}
